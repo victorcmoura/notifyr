d54d756d8bf230a85a47131b3973eb876fe25292
Victor Moura <victor_cmoura@hotmail.com>
2018-11-12 09:26:48 -0200

    Let there be light

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='notifyr',
+      version='0.1',
+      description='Object notification tool',
+      url='https://github.com/victorcmoura/notifyr',
+      author='Victor Moura',
+      author_email='victor_cmoura@hotmail.com',
+      license='MIT',
+      packages=['notifyr'],
+      zip_safe=False)
