77c55be75185a76d97229ecd72e5e4d3a38008a5
Victor Moura <victor_cmoura@hotmail.com>
2018-11-15 12:12:47 -0200

    Adds observer class decorator

--- notifyr/agents.py
+++ notifyr/agents.py
@@ -1,3 +1,19 @@
+def observer(update_function):
+
+    update_function = 'args[0].' + update_function + '(*args[1:], **kwargs)' # Object.update_function(Object, args)
+
+    def class_decorator(cls):
+        def update(*args, **kwargs):
+            eval(update_function)
+
+        def wrapper(cls):
+            setattr(cls, 'update', update)
+            return cls
+
+        return wrapper(cls)
+
+    return class_decorator
+
 def observed(observers_list_function):
 
     pre_eval_observers_list = 'args[0].' + observers_list_function + '()' # Object.observers_list_function()
