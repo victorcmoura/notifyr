0924cb5501761be8162ef43bfd111b5c9ede32a3
Victor Moura <victor_cmoura@hotmail.com>
2018-11-15 13:02:00 -0200

    Fixes example so it matches last implemented feature

--- example.py
+++ example.py
@@ -1,11 +1,10 @@
 from notifyr.agents import observer, observed
 from notifyr.functions import target
 
-@observed('get_owners')
+@observed
 class Dog(object):
     def __init__(self, name):
         self.name = name
-        self.owners = []
 
     @target
     def bark(self):
@@ -13,12 +12,6 @@ class Dog(object):
 
     def sleep(self):
         print('ZZzzZZZzZzZ')
-
-    def get_owners(self):
-        return self.owners
-
-    def add_owner(self, owner):
-        self.owners.append(owner)
 
 @observer('educate_dog')
 class Owner(object):
@@ -34,6 +27,6 @@ d = Dog('Tobby')
 d.bark()
 
 o = Owner('Victor')
-d.add_owner(o)
+d.attach(o)
 
 d.bark()
